bb5e172af6b19b7ac067963d5ba49ba711ba16f0
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-10 16:40:55 +0200

    Default User added + image

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,5 +1,7 @@
 import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { MenuIcon } from "lucide-react";
+import DefaultUser from "../../public/DefaultUser.png";
+import Image from "next/image";
 
 export function UserNav () {
     return (
@@ -7,6 +9,8 @@ export function UserNav () {
             <DropdownMenuTrigger>
                 <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
                     <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5"/>
+
+                    <Image src={DefaultUser} alt="Default User" className="w-8 h-8 rounded-full hidden lg:block" />
                 </div>
             </DropdownMenuTrigger>
         </DropdownMenu>
