7bc221fb3282e8d8a19883048e8f7403fc833d85
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-10 16:10:10 +0200

    Desktop Logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import DesktopLogo from "../../public/airbnb-desktop.png"
+// import MobileLogo from "../../public/airbnb-mobile.png"
 
 export function Navbar() {
     return (
         <nav className="w-full border-b">
-<div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
-<Link href="/">
-<Image src={} />
-</Link>
-</div>
+            <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+                <Link href="/">
+                    <Image src={DesktopLogo} alt="Desktop Logo" className="w-42" />
+                </Link>
+            </div>
         </nav>
     )
 }
