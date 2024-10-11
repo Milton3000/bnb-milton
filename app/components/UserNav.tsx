a54926964b3219725a74bc6a7d273bff8e2839ca
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-11 11:11:49 +0200

    Google / User Image added, added to next.config too

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -16,7 +16,7 @@ export async function UserNav() {
                 <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
                     <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
 
-                    <Image src={DefaultUser} alt="Default User" className="w-8 h-8 rounded-full hidden lg:block" />
+                    <Image src={user?.picture ?? DefaultUser} alt="User" width={32} height={32} className="w-8 h-8 rounded-full hidden lg:block" />
                 </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-[200px]">
