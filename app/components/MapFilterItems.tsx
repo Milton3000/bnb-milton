0206c1976f7034d29a8be7631b093f7aa92a2f65
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-12 19:29:15 +0200

    MapFilter component + import

---
+++ app/components/MapFilterItems.tsx
@@ -0,0 +1,7 @@
+export function MapFilterItems () {
+    return (
+        <div>
+            Hello from Filter
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { MapFilterItems } from "./components/MapFilterItems";
 
 export default function Home() {
-return <div>
-  <Button> Click me </Button>
+return <div className="container mx-auto px-5 lg:px-10">
+<MapFilterItems />
 </div>
 }
