4bbecae8939d576fb0b0d6252dd1c773280730c1
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-12 20:02:52 +0200

    css fix + start with useSearch + Pathname

--- app/components/MapFilterItems.tsx
+++ app/components/MapFilterItems.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import { categoryItems } from "../lib/categoryItems";
 import Image from "next/image";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export function MapFilterItems () {
+    const searchParams = useSearchParams();
+    const search = searchParams.get("filter");
+    const pathname = usePathname();
+
     return (
-        <div>
-            {categoryItems.map((item) => (
+        <div className="flex gap-x-10 mt-5 w-full overflow-x-scroll no-scrollbar">
+            {categoryItems.map((item) => 
                 <Link key={item.id} href="">
                 <div className="relative w-6 h-6">
                     <Image src={item.imageUrl} alt="Category Image" className="w-6 h-6"
@@ -13,8 +20,9 @@ export function MapFilterItems () {
                     height={24}
                     />
                 </div>
+                <p className="text-xs font-medium"> {item.title}</p>
                 </Link>
-            ))}
+            )}
         </div>
     );
 }
