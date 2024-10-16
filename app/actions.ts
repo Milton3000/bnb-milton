7dff1ba29ecea4b84f055920cafc6a3457ed89eb
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-16 16:41:22 +0200

    actions.ts (check for existing homes etc)

---
+++ app/actions.ts
@@ -0,0 +1,30 @@
+"use server"
+
+import { redirect } from "next/navigation";
+import prisma from "./lib/db"
+
+// Server environment "use server"
+// File for the server actions, from Next.Js 13.4
+
+
+// Check for existing homes etc. 
+export async function createHome({ userId }: { userId: string }) {
+    const data = await prisma.home.findFirst({
+        where: {
+            userId: userId
+        },
+        orderBy: {
+            createdAt: "desc",
+        }
+    });
+
+    // If the data has zero homes in the db, then create a new home.
+    if (data === null) {
+        const data = await prisma.home.create({
+            data: {
+                userId: userId,
+            },
+        });
+        return redirect(`/create/${data.id}/structure`);
+    }
+}
