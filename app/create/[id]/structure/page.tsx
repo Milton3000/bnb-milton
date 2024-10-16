142bd32dade84f27bb6603283215015c050bdb16
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-16 14:18:43 +0200

    page.tsx for structure start

---
+++ app/create/[id]/structure/page.tsx
@@ -0,0 +1,17 @@
+export default function StructureRoute() {
+    return (
+<>
+<div className="w-3/5 mx-auto">
+
+<h2 className="text-3xl font-semibold tracking-tight transition-colors">
+    Which of these best describe your Home? 
+</h2>
+</div>
+
+<form>
+    
+
+</form>
+</>
+    )
+}
