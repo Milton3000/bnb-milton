70e6f83a6fbcf4b19ce35e8c0e744839014a1a80
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-19 23:37:49 +0200

    Fix

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 // If we use State in any form, we have to mark it as "use client" with Next.js
 import { useFormStatus } from "react-dom"
 
@@ -9,9 +10,14 @@ export function SubmitButton() {
     return (
         <>
             {pending ? (
-                <Button type="submit" size="lg">Next</Button>
+                <Button disabled size="lg">
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    Please Wait
+                </Button>
             ) : (
-                <Button type="submit" size="lg">Next</Button>
+                <Button type="submit" size="lg">
+                    Next
+                </Button>
             )}
         </>
     )
