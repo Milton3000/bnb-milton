39e33a1e271a975ed5b0bb5fe50fe7c5642fb12e
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-20 21:49:23 +0200

    Select + Select Countries start

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,5 +1,33 @@
+import { Select, SelectContent, SelectGroup, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+
 export default function AddressRoute() {
     return (
-        <h1>Address Route</h1>
+        <>
+            <div className="w-3/5 mx-auto">
+                <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
+                    Where is your Home located?
+                </h2>
+            </div>
+
+            <form>
+
+                <div className="w-3/5 mx-auto">
+                    <div className="mb-5">
+                    <Select required>
+                    <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a Country" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>
+                                Countries
+                            </SelectLabel>
+                        </SelectGroup>
+                    </SelectContent>
+                    </Select>
+                    </div>
+                </div>
+            </form>
+        </>
     )
 }
