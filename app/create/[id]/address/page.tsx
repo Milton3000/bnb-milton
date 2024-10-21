58b86996e1f8aa2782a92c5d2d041bed2977c06f
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-21 20:41:49 +0200

    Map installed + LazyMap activated

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,38 +1,58 @@
+import { BottomBar } from "@/app/components/BottomBar";
 import { useCountries } from "@/app/lib/getCountries";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
+import dynamic from "next/dynamic";
 
 export default function AddressRoute() {
-    const { getAllCountries } = useCountries();
-    return (
-        <>
-            <div className="w-3/5 mx-auto">
-                <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
-                    Where is your Home located?
-                </h2>
-            </div>
+  const { getAllCountries } = useCountries();
 
-            <form>
+const LazyMap = dynamic(() => import("@/app/components/Map"), { ssr: false,
+    loading: () => <Skeleton className="h-[50vh] w-full" />
 
-                <div className="w-3/5 mx-auto">
-                    <div className="mb-5">
-                        <Select required>
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Select a Country" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                    <SelectLabel> Countries </SelectLabel>
-                                    {getAllCountries().map((item) => (
-                                        <SelectItem key={item.value} value={item.value}>
-                                            {item.flag} {item.label} / {item.region}
-                                        </SelectItem>
-                                    ))}
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </div>
-                </div>
-            </form>
-        </>
-    )
+});
+
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
+          Where is your Home located?
+        </h2>
+      </div>
+
+      <form>
+        <div className="w-3/5 mx-auto mb-36">
+          <div className="mb-5">
+            <Select required>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a Country" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel> Countries </SelectLabel>
+                  {getAllCountries().map((item) => (
+                    <SelectItem key={item.value} value={item.value}>
+                      {item.flag} {item.label} / {item.region}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <LazyMap />
+        </div>
+
+<BottomBar />
+
+      </form>
+    </>
+  );
 }
