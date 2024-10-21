6b1aecab2f862cfd48f1912ddbfe6196c45c0538
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-21 20:49:01 +0200

    zoom fix

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -7,13 +7,13 @@ import { useCountries } from "../lib/getCountries";
 export default function Map({locationValue}: {locationValue: string}) {
     const { getCountryByValue } = useCountries();
     const latLang = getCountryByValue(locationValue)?.latLang;
-    
+
     return (
         <MapContainer
-            scrollWheelZoom={false}
+            scrollWheelZoom={true}
             className="h-[50vh] rounded-lg relative z-0"
             center={latLang ?? [52.505, -0.09]}
-            zoom={13}
+            zoom={10}
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
