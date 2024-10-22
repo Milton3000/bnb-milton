ec4a326c2ce215d52a47277feaf6cac88f5ce3a7
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-22 20:21:21 +0200

    else if statements for address

--- app/actions.ts
+++ app/actions.ts
@@ -1,111 +1,126 @@
-"use server"
+"use server";
 
 import { redirect } from "next/navigation";
-import prisma from "./lib/db"
+import prisma from "./lib/db";
 import { supabase } from "./lib/supabase";
 
 // Server environment "use server"
 // File for the server actions, from Next.Js 13.4
 
-
-// Check for existing homes etc. 
+// Check for existing homes etc.
 export async function createHome({ userId }: { userId: string }) {
-    const data = await prisma.home.findFirst({
-        where: {
-            userId: userId
-        },
-        orderBy: {
-            createdAt: "desc",
-        }
+  const data = await prisma.home.findFirst({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  // If the data has zero homes in the db, then create a new home.
+  if (data === null) {
+    const data = await prisma.home.create({
+      data: {
+        userId: userId,
+      },
     });
-
-    // If the data has zero homes in the db, then create a new home.
-    if (data === null) {
-        const data = await prisma.home.create({
-            data: {
-                userId: userId,
-            },
-        });
-        return redirect(`/create/${data.id}/structure`);
-    } else if (!data.addedCategory && !data.addedDescription && !data.addedLocation) {
-        return redirect(`/create/${data.id}/structure`);
-    } else if (data.addedCategory && !data.addedDescription) {
-        return redirect(`/create/${data.id}/description`);
-    }
+    return redirect(`/create/${data.id}/structure`);
+  } else if (
+    !data.addedCategory &&
+    !data.addedDescription &&
+    !data.addedLocation
+  ) {
+    return redirect(`/create/${data.id}/structure`);
+  } else if (data.addedCategory && !data.addedDescription) {
+    return redirect(`/create/${data.id}/description`);
+  } else if (
+    data.addedCategory &&
+    data.addedDescription &&
+    !data.addedLocation
+  ) {
+    return redirect(`/create/${data.id}/address`);
+  } else if (
+    data.addedCategory &&
+    data.addedDescription &&
+    data.addedLocation
+  ) {
+    const data = await prisma.home.create({
+      data: {
+        userId: userId,
+      },
+    });
+    return redirect(`/create/${data.id}/structure`);
+  }
 }
-
 
 export async function createCategoryPage(formData: FormData) {
+  const categoryName = formData.get("categoryName") as string;
+  const homeId = formData.get("homeId") as string;
 
+  const data = await prisma.home.update({
+    where: {
+      id: homeId,
+    },
+    data: {
+      categoryName: categoryName,
+      addedCategory: true,
+    },
+  });
 
-    const categoryName = formData.get("categoryName") as string;
-    const homeId = formData.get("homeId") as string;
-
-    const data = await prisma.home.update({
-        where: {
-            id: homeId,
-        },
-        data: {
-            categoryName: categoryName,
-            addedCategory: true,
-        },
-    });
-
-    return redirect(`/create/${homeId}/description`);
+  return redirect(`/create/${homeId}/description`);
 }
 
-
 export async function createDescriptionPage(formData: FormData) {
-    const title = formData.get("title") as string;
-    const description = formData.get("description") as string;
-    const price = formData.get("price");
-    const image = formData.get("image") as File;
-    const homeId = formData.get("homeId") as string;
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const price = formData.get("price");
+  const image = formData.get("image") as File;
+  const homeId = formData.get("homeId") as string;
 
-    const guestNumber = formData.get("guests") as string;
-    const roomNumber = formData.get("room") as string;
-    const bathroomNumber = formData.get("bathroom") as string;
+  const guestNumber = formData.get("guests") as string;
+  const roomNumber = formData.get("room") as string;
+  const bathroomNumber = formData.get("bathroom") as string;
 
-    const { data: imageData } = await supabase.storage.from
-    ("images").upload(`${image.name}-${new Date()}`, image,
-        {
-            cacheControl: "2592000",
-            contentType: "image/png",
-        });
+  const { data: imageData } = await supabase.storage
+    .from("images")
+    .upload(`${image.name}-${new Date()}`, image, {
+      cacheControl: "2592000",
+      contentType: "image/png",
+    });
 
-        const data = await prisma.home.update({
-            where: {
-                id: homeId,
-            },
-            data: {
-                title: title,
-                description: description,
-                price: Number(price),
-                bedrooms: roomNumber,
-                bathrooms: bathroomNumber,
-                guests: guestNumber,
-                photo: imageData?.path,
-                addedDescription: true,
-            },
-        });
+  const data = await prisma.home.update({
+    where: {
+      id: homeId,
+    },
+    data: {
+      title: title,
+      description: description,
+      price: Number(price),
+      bedrooms: roomNumber,
+      bathrooms: bathroomNumber,
+      guests: guestNumber,
+      photo: imageData?.path,
+      addedDescription: true,
+    },
+  });
 
-        return redirect(`/create/${homeId}/address`);
+  return redirect(`/create/${homeId}/address`);
 }
 
 export async function createLocation(formData: FormData) {
-    
-    const homeId = formData.get("homeId") as string;
-    const countryValue = formData.get("countryValue") as string;
+  const homeId = formData.get("homeId") as string;
+  const countryValue = formData.get("countryValue") as string;
 
-    const data = await prisma.home.update({
-        where: {
-            id: homeId,
-        },
-        data: { 
-            country: countryValue,
-            addedLocation: true,
-        },
-    });
+  const data = await prisma.home.update({
+    where: {
+      id: homeId,
+    },
+    data: {
+      country: countryValue,
+      addedLocation: true,
+    },
+  });
 
-    return redirect("/");
+  return redirect("/");
 }
