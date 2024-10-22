3e39ca303e19cc2e92915590c21956fd853f371d
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-22 20:26:53 +0200

    ListingCard comp started + imported to page

---
+++ app/components/ListingCard.tsx
@@ -0,0 +1,7 @@
+export function ListingCard() {
+    return (
+        <div>
+            <h1> Hello from Card </h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import { ListingCard } from "./components/ListingCard";
 import { MapFilterItems } from "./components/MapFilterItems";
+import prisma from "./lib/db";
 
-export default function Home() {
-return <div className="container mx-auto px-5 lg:px-10">
-<MapFilterItems />
-</div>
+async function getData() {
+  const data = await prisma.home.findMany({
+    where: {
+      addedCategory: true,
+      addedLocation: true,
+      addedDescription: true,
+    },
+    select: {
+      photo: true,
+      id: true,
+      price: true,
+      description: true,
+    },
+  });
+  return data;
+}
+
+export default async function Home() {
+    const data = await getData();
+
+  return (
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
+      
+
+      <div>
+        {data.map((item) => (
+            <ListingCard key={item.id}/>
+        ))}
+      </div>
+    </div>
+  );
 }
