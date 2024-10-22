442e20f36ddf446c830f8b0dccd4a868ea182098
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-22 18:30:57 +0200

    marker added to map

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -3,6 +3,15 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useCountries } from "../lib/getCountries";
+import { icon } from "leaflet";
+
+const ICON = icon({
+    iconUrl: "https://static.vecteezy.com/system/resources/thumbnails/019/897/155/small/location-pin-icon-map-pin-place-marker-png.png",
+    iconSize: [50, 50],
+    iconAnchor: [12, 41],
+    shadowSize: [41, 41],
+    shadowAnchor: [50, 41]
+})
 
 export default function Map({locationValue}: {locationValue: string}) {
     const { getCountryByValue } = useCountries();
@@ -19,6 +28,8 @@ export default function Map({locationValue}: {locationValue: string}) {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
+            <Marker position={latLang ?? [52.505, -0.09]} icon={ICON}/>
         </MapContainer>
     );
 }
