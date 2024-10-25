cc5e47da9afe9876953acc2d252c76bc24690ad3
Milton3000 <kristofferssonmilton@gmail.com>
2024-10-25 15:42:29 +0200

    Logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import DesktopLogo from "../../public/airbnb-desktop.png"
 import MobileLogo from "../../public/airbnb-mobile.webp"
+import dreambnb from "../../public/dreambnb.png"
 import { UserNav } from "./UserNav";
 
 export function Navbar() {
@@ -9,9 +10,9 @@ export function Navbar() {
         <nav className="w-full border-b">
             <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
                 <Link href="/">
-                    <Image src={DesktopLogo} alt="Desktop Logo" className="w-32 hidden lg:block" />
+                    <Image src={dreambnb} alt="Desktop Logo" className="w-32 hidden lg:block" />
 
-                    <Image src={MobileLogo} alt="Mobile Logo" className="block lg:hidden w-12" />
+                    <Image src={dreambnb} alt="Mobile Logo" className="block lg:hidden w-12" />
                 </Link>
                 <div className="rounded-full border px-5 py-2">
                     <h1>Start with Search</h1>
