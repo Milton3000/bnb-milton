fc60deb2c8c303ba405fe3330c8c4c968aff66aa
Milton3000 <kristofferssonmilton@gmail.com>
2024-11-02 13:42:48 +0100

    DialogContent start + form

--- app/components/SearchComponent.tsx
+++ app/components/SearchComponent.tsx
@@ -1,4 +1,5 @@
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { SearchIcon } from "lucide-react";
 
 export function SearchModalComponent() {
   return (
@@ -10,8 +11,14 @@ export function SearchModalComponent() {
             <p className="px-4">Any Week</p>
             <p className="px-4">Add Guests</p>
           </div>
+          <SearchIcon className="bg-primary text-white p-1 h-8 w-8 rounded-full"/>
         </div>
       </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+    <form className="gap-4 flex flex-col">
+    
+    </form>
+      </DialogContent>
     </Dialog>
   );
 }
