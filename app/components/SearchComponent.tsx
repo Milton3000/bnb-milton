de45949802ede06b17c75ddf2c68b7c1eb2c33ed
Milton3000 <kristofferssonmilton@gmail.com>
2024-11-02 13:47:08 +0100

    Dialog Header, TItle + Select taken from Address Page (same), imports, state + hook

--- app/components/SearchComponent.tsx
+++ app/components/SearchComponent.tsx
@@ -1,7 +1,29 @@
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { SearchIcon } from "lucide-react";
+import { useState } from "react";
+import { useCountries } from "../lib/getCountries";
 
 export function SearchModalComponent() {
+  const [step, useStep] = useState(1);
+  const [locationValue, setLocationValue] = useState("");
+  const { getAllCountries } = useCountries();
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -11,13 +33,42 @@ export function SearchModalComponent() {
             <p className="px-4">Any Week</p>
             <p className="px-4">Add Guests</p>
           </div>
-          <SearchIcon className="bg-primary text-white p-1 h-8 w-8 rounded-full"/>
+          <SearchIcon className="bg-primary text-white p-1 h-8 w-8 rounded-full" />
         </div>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-    <form className="gap-4 flex flex-col">
-    
-    </form>
+        <form className="gap-4 flex flex-col">
+          {step === 1 ? (
+            <>
+              <DialogHeader>
+                <DialogTitle> Select a Country </DialogTitle>
+                <DialogDescription>Please choose a Country</DialogDescription>
+              </DialogHeader>
+
+              <Select
+                required
+                onValueChange={(value) => setLocationValue(value)}
+                value={locationValue}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a Country" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel> Countries </SelectLabel>
+                    {getAllCountries().map((item) => (
+                      <SelectItem key={item.value} value={item.value}>
+                        {item.flag} {item.label} / {item.region}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </>
+          ) : (
+            <div></div>
+          )}
+        </form>
       </DialogContent>
     </Dialog>
   );
